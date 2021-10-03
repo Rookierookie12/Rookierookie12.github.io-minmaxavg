83d8cc18f18ffec3fb604947ab335fdbb0e937e7
Cass Gan 2 <ganaw0914@gmail.com>
2021-10-03 23:54:27 +0800

    0310

---
+++ minmaxavg/minmaxavg.js
@@ -0,0 +1,16 @@
+var arr = [2, 15, 8]
+var max = arr[0];
+var min = arr[0];
+var sum = arr[0]; 
+for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+    sum = sum + arr[i];
+}
+console.log('max=' +max)
+console.log('min='  +min)
+console.log('avg=' +sum/arr.length) 
